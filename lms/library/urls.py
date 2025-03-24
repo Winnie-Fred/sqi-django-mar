af65b7d274929942b11c5fc29dfc19960231cefd
Tubo Jesu <jesulayomioguntubo@gmail.com>
2025-03-24 07:49:53 -0400

    teach how to use forms.Form instead of forms.ModelForm

--- lms/library/urls.py
+++ lms/library/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('add-book-no-django-form/', views.add_book_no_django_form, name="add_book_no_django_form"),
     path('add-book-with-django-form/', views.add_book_with_django_form, name="add_book_with_django_form"),
     path('add-book-with-django-form-manual-render/', views.add_book_with_django_form_manual_render, name="add_book_with_django_form_manual_render"),
+    path('add-book-with-django-forms-dot-form/', views.add_book_with_django_forms_dot_form, name="add_book_with_django_forms_dot_form"),
 ]
--- lms/library/views.py
+++ lms/library/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 from authors.models import Author
 from .models import Book
-from .forms import BookForm
+from .forms import BookForm, BookManualForm
 
 # Create your views here.
 def all_authors(request):
@@ -71,3 +71,24 @@ def add_book_with_django_form_manual_render(request):
         "authors": all_authors,
     }
     return render(request, "library/manual-form-plus-django-form.html", context)
+
+
+def add_book_with_django_forms_dot_form(request):
+    form = BookManualForm()
+
+    if request.method == "POST":
+        form = BookManualForm(request.POST, request.FILES)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            title = cleaned_data.get('title')
+            author = cleaned_data.get('author')
+            number_of_pages = cleaned_data.get('number_of_pages')
+            published_on = cleaned_data.get('published_on')
+            cover_image = cleaned_data.get('cover_image')
+            Book.objects.create(title=title, author=author, number_of_pages=number_of_pages, published_on=published_on, cover_image=cover_image)
+            return redirect('library:all_books')
+
+    context = {
+        "form": form
+    }
+    return render(request, "library/add-book-with-django-forms-dot-form.html", context)
